0633fa4ad6d990e9159570f11951693a606e15f1
Noahkimtai <noahkimtai24@gmail.com>
2022-08-08 14:56:45 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function countDown(int){
+    for (let i=0;i<=int;i++){
+        console.log(i)
+    }
+}
+
+function writeCards(name,eventName){
+    let newArray =[]
+    for (let i=0;i<name.length;i++){
+         newArray.push(`Thank you, ${name[i]}, for the wonderful ${eventName} gift!`)
+    }
+    return newArray
+}
+countDown(10)
+writeCards(["Guadalupe", "Ollie", "Aki"],'suprise')
